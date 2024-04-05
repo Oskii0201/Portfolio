650b68f6e4f3db1bd1f2c11e1678ae0f16c6643d
Oskar <oskar0201@onet.pl>
2024-04-05 23:44:46 +0200

    updated: About component added: image

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,10 +1,15 @@
-import styles from "./About.module.css"
 import {Container} from "../Container/Container.jsx";
 import {Description} from "./Description/Description.jsx";
+import {TechStack} from "./TechStack/TechStack.jsx";
+import {SoftSkills} from "./SoftSkills/SoftSkills.jsx";
+import {ToolStack} from "./ToolStack/ToolStack.jsx";
 export function About(){
     return(
         <Container>
             <Description/>
+            <SoftSkills/>
+            <TechStack/>
+            <ToolStack/>
         </Container>
     )
 }
